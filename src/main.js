c875377e14182c4183e657527770fbffc04aa9ad
chenmi <jack.chenyuana@gmail.com>
2022-01-09 12:20:33 +0800

    修复一些小问题

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router';
-import axios from 'axios'
-import VueAxios from 'vue-axios'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios);
 Vue.use(ElementUI);
 
 new Vue({
